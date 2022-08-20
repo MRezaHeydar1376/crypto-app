6768c7b0b906f6b48f4726072df58ad5bff07bf2
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-08-20 12:07:24 -0700

    Create page Home and add Api

---
+++ src/pages/home.tsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+
+const API = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+
+function Home () {
+
+    useEffect(() => {
+        axios
+        .get(API)
+        .then(response => {
+            console.log(response.data)
+        })
+        .catch(error => {
+            console.log(Error)
+        })
+    }, [])
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Home;
