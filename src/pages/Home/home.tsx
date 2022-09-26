d4e8134a7f314460f4db6059b15a3066456e4c4e
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-09-26 01:48:03 -0700

    Import React and ReactDom

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect } from "react";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const API = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
