6e867b3fbf53e91d33bb8d2a07d363ed533e23fb
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-09-26 01:47:10 -0700

    Create Topnavbar

--- src/layout/TopNavbar/index.tsx
+++ src/layout/TopNavbar/index.tsx
@@ -1,10 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Brand, FlagIran } from "../../assets";
+import { ButtonDropDown } from "../../components";
+import { ITEM } from '../../components/button_dropdown';
+import { Div } from '../../components/Global/style';
 
+const Items = [
+    {text: "Persian", id: "1"},
+    {text: "English", id: "2"}
+]
 
-function TopNavbar () {
+function TopNavbar() {
     return (
-        <div>
-            
-        </div>
+        <Div display="flex" align="center">
+            <Div size="20%" display="flex" justify="start">
+                <img src={Brand} alt="" />
+            </Div>
+            <Div size="80%" display="flex" justify="end">
+                <ButtonDropDown
+                    title="Language"
+                    icon={FlagIran}
+                    items={Items}
+                />
+            </Div>
+        </Div>
     )
 }
 
