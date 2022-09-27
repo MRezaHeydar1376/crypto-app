79ca89646f886994a9902bfa16e1b296531cddce
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-09-27 12:25:09 -0700

    Create component ButtonDropdown

--- src/components/button_dropdown/index.tsx
+++ src/components/button_dropdown/index.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useState } from 'react';
 import { memo, ReactElement } from "react";
-import { Div, H6, Paragraph } from '../../styles';
+import { Button, Div, H6, Img, Paragraph } from '../../styles';
 
 export type ITEM = {
     text: string;
@@ -11,11 +11,13 @@ interface Props {
     icon?: string;
     title: string;
     items: ITEM[];
+    selectItem(text: string, id: string): void;
 }
 
-function ButtonDropDown({ icon, title, items }: Props): ReactElement {
+function ButtonDropDown({ icon, title, items, selectItem }: Props): ReactElement {
 
     const [dropdown, setDropdown] = useState(false);
+    const [language, setLanguage] = useState("English");
 
     const openDropdown = useCallback(() => {
         setDropdown(!dropdown);
@@ -23,25 +25,70 @@ function ButtonDropDown({ icon, title, items }: Props): ReactElement {
 
     const closeDropdown = () => {
         setDropdown(false);
-    }
-    console.log(items)
+    };
+
+    const selectLanguage = (id: string) => {
+        if (id === "1") {
+            setLanguage("English")
+            setDropdown(false);
+            selectItem("English", "1")
+        } else {
+            setLanguage("فارسی")
+            setDropdown(false);
+            selectItem("فارسی", "2")
+        }
+    };
+
     return (
-        <Div>
-            <button onClick={openDropdown}>
-                {icon ? (
-                    <img src={icon} alt="" />
-                ) : null}
-                <H6>
-                    {title}
+        <Div
+            display="flex"
+            align="center"
+            direction="column"
+            position="relative"
+            width="10%"
+        >
+            <Button
+                onClick={openDropdown}
+                borderRadius="8px"
+                display="flex"
+                align="center"
+                width="100%"
+                justify="space-between"
+            >
+                <H6 fontSize="13px">
+                    {language}
                 </H6>
-            </button>
-            <Div>
-                {items ? items.map(item => (
-                    <Paragraph key={item.id}>
-                        {item.text}
-                    </Paragraph>
-                )) : null}
-            </Div>
+                {icon ? (
+                    <Img
+                        src={icon}
+                        alt=""
+                        borderRadius="100%"
+                        width="30%"
+                    />
+                ) : null}
+            </Button>
+            {dropdown ?
+                <Div
+                    position="absolute"
+                    top="35px"
+                    display="flex"
+                    align="ccenter"
+                    direction="column"
+                >
+                    {items ? items.map(item => (
+                        <Paragraph
+                            key={item.id}
+                            onClick={() => selectLanguage(item.id)}
+                            margin="10px auto"
+                            fontSize="12px"
+                            cursor="pointer"
+                        >
+                            {item.text}
+                        </Paragraph>
+                    )) : null}
+                </Div> :
+                null
+            }
         </Div>
     )
 }
