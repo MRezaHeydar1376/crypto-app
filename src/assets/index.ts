24d980597deb44184e5d6fe51564a2060f1941e7
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-09-29 04:02:29 -0700

    Add new icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
 export { default as FlagIran } from "./icons/iran_flag.svg";
 export { default as FlagUsa } from "./icons/USA-Flag.svg";
 export { default as Brand } from "./images/brand.png";
+export { default as Search } from "./icons/search.svg";
