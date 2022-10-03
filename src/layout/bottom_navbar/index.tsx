6381c870876297fac0f0e2b9a98d69cdc7a1c295
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-10-03 09:55:58 -0700

    Create layout BottomNavbar

---
+++ src/layout/bottom_navbar/index.tsx
@@ -0,0 +1,20 @@
+import { InputSearch } from "../../components";
+import { Div } from "../../styles";
+
+function BottomNavbar() {
+    return (
+        <Div>
+            <Div width="50%">
+
+            </Div>
+            <Div width="25%">
+
+            </Div>
+            <Div width="25%">
+                <InputSearch />
+            </Div>
+        </Div>
+    );
+}
+
+export default BottomNavbar;
