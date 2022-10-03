41993e67e27f1289c8c53dfd34ddfc0bd844b87d
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-10-03 09:54:45 -0700

    Add new imgae and icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,6 @@
 export { default as FlagIran } from "./icons/iran_flag.svg";
 export { default as FlagUsa } from "./icons/USA-Flag.svg";
 export { default as Brand } from "./images/brand.png";
-export { default as Search } from "./icons/search.svg";
+export { default as SearchBlack } from "./icons/search-black.svg";
+export { default as SearchWhite } from "./icons/search-white.svg";
+export { default as Luna } from "./images/image-luna.png";
