cbf3fe3fbe1fea4ffc1a49b751c91ce0c808a1eb
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-10-03 10:02:11 -0700

    Rename this file

---
+++ src/layout/top_navbar/index.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { Brand, FlagIran, FlagUsa } from "../../assets";
+import { ButtonDropDown } from "../../components";
+import { Border, Div, Img } from "../../styles";
+
+const Items = [
+    { text: "English", id: "1" },
+    { text: "فارسی", id: "2" },
+]
+
+function TopNavbar() {
+    const [icon, setIcon] = useState(FlagUsa);
+
+    function selectItem(text: string, id: string) {
+        console.log(id)
+        if (id === "1") {
+            setIcon(FlagUsa)
+        } else {
+            setIcon(FlagIran)
+        }
+    }
+
+    return (
+        <Div>
+            <Div display="flex" justify="space-between" align="center">
+                <Div width="20%" display="flex" justify="start">
+                    <Img
+                        src={Brand}
+                        alt=""
+                        width="75%"
+                    />
+                </Div>
+                <Div width="80%" display="flex" justify="end">
+                    <ButtonDropDown
+                        title="Language"
+                        icon={icon}
+                        items={Items}
+                        selectItem={selectItem}
+                    />
+                </Div>
+            </Div>
+            <Border height="1.5px" />
+        </Div>
+    )
+}
+
+export default TopNavbar;
