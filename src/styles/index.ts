d23d3275a627bd9c1f07162ee7bb01e486644a22
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-10-28 11:10:24 -0700

    Update interface PropsDiv

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -13,11 +13,14 @@ interface PropsDiv {
     left?: string;
     backgroundColor?: string;
     boxShadow?: string;
+    border?: string;
     borderRadius?: string;
     padding?: string;
     overflowY?: string;
     overflowX?: string;
     margin?: string;
+    zIndex?: string;
+    cursor?: string;
 }
 
 interface PropsText {
@@ -87,11 +90,15 @@ export const Div = styled.div<PropsDiv>`
     flex-direction: ${({ direction = "row" }) => (direction ? direction : "row")};
     position: ${({ position = "block" }) => (position ? position : "row")};
     top: ${({ top = "0px" }) => (top ? top : "0px")};
+    left: ${({ left = "none" }) => (left? left:"none")};
     padding: ${({ padding = "0px" }) => (padding ? padding : "0px")};
     background-color: ${({ backgroundColor = "transparent" }) => (backgroundColor ? backgroundColor : "transparent")};
     box-shadow: ${({ boxShadow = "none" }) => (boxShadow ? boxShadow : "none")};
     border-radius: ${({ borderRadius = "0px" }) => (borderRadius ? borderRadius : "0px")};
     margin: ${({ margin = "0px" }) => (margin ? margin : "0px")};
+    border: ${({ border = "none" }) => (border ? border : "none")};
+    z-index: ${({ zIndex = "1" }) => (zIndex ? zIndex : "1")};
+    cursor: ${({ cursor = "auto" }) => (cursor ? cursor : "auto")};
     overflow-y: ${({ overflowY = "visible" }) => (overflowY ? overflowY : "visible")};
     overflow-x: ${({ overflowX = "visible" }) => (overflowX ? overflowX : "visible")};
     &::-webkit-scrollbar {
