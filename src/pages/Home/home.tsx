ddd3e34c42dcc95b5b1d572a9cc5f05a7d43f2e7
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-10-31 12:50:53 -0700

    Add component Tab and Border

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,25 +1,40 @@
 import axios from "axios";
 import { useEffect } from "react";
+import { Tab } from "../../components";
+import { Border, Div } from "../../styles";
+import { Color } from "../../variable";
 
 const API = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
-function Home () {
+const Name_Tab = [
+    { name: "Coins", id: "0" },
+    { name: "New Coins", id: "1" },
+    { name: "Gainers & Losers", id: "2" },
+    { name: "Categories", id: "3" },
+    { name: "NFT", id: "4" },
+    { name: "Gaming", id: "5" },
+]
+
+function Home() {
 
     useEffect(() => {
         axios
-        .get(API)
-        .then(response => {
-            console.log(response.data)
-        })
-        .catch(error => {
-            console.log(Error)
-        })
+            .get(API)
+            .then(response => {
+                console.log(response.data)
+            })
+            .catch(error => {
+                console.log(Error)
+            })
     }, [])
 
-    return(
-        <div>
-
-        </div>
+    return (
+        <Div>
+            <Div width="60%">
+                <Tab links={Name_Tab} />
+            </Div>
+            <Border height="0.1px" color={Color.gray}/>
+        </Div>
     )
 }
 
