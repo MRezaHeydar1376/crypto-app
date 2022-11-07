71070f165a068b5f85d8cb9aac00e695e54b56ac
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-11-07 07:16:01 -0800

    Add new icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,3 +4,5 @@ export { default as Brand } from "./images/brand.png";
 export { default as SearchBlack } from "./icons/search-black.svg";
 export { default as SearchWhite } from "./icons/search-white.svg";
 export { default as Luna } from "./images/image-luna.png";
+export { default as StarWhite } from "./icons/star-white.svg";
+export { default as StarGold } from "./icons/star-gold.svg";
